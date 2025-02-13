f5736448c15ed240c1530230ac9f5fb18ccb1899
Danny Carey <carey.d@ntlworld.com>
2025-02-13 11:15:08 +0000

    Step 4 completed. Updated NotePage with the LoadNote() method and removed the 'if' statement. xaml file also updated.

---
+++ Views/NotePage.xaml.cs
@@ -0,0 +1,46 @@
+namespace Notes.Views;
+
+public partial class NotePage : ContentPage
+{
+	string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
+	public NotePage()
+	{
+		InitializeComponent();
+
+    // if (File.Exists(_fileName))
+    //     TextEditor.Text = File.ReadAllText(_fileName);
+	// }
+
+	string appDataPath = FileSystem.AppDataDirectory;
+	string randomFileName = $"{Path.GetRandomFileName()}.notes.txt";
+	
+	LoadNote(Path.Combine(appDataPath, randomFileName));
+	}
+	private void OnSaveButtonClicked(object sender, EventArgs e)
+	{
+		File.WriteAllText(_fileName, TextEditor.Text);
+	}
+
+	private void DeleteButton_Clicked(object sender, EventArgs e)
+{
+    // Delete the file.
+    if (File.Exists(_fileName))
+        File.Delete(_fileName);
+
+    TextEditor.Text = string.Empty;
+}
+
+	private void LoadNote(string filename)
+	{
+		Models.Note noteModel = new Models.Note();
+		noteModel.Filename = filename;
+
+		if (File.Exists(filename))
+		{
+			noteModel.Text = File.ReadAllText(filename);
+			noteModel.Date = File.GetCreationTime(filename);
+		}
+		BindingContext = noteModel;
+	}
+
+}
